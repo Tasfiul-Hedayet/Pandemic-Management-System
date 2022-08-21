b0e15a32f7791103bf1053c0fa0b1d00e3012b30
Abir Anjum Prottoy <62479082+f0rGoT-Ten@users.noreply.github.com>
2022-08-22 02:40:01 +0600

    update

--- form.php
+++ form.php
@@ -1,8 +1,6 @@
 <?php
 $username = filter_input(INPUT_POST, 'username');
 $password = filter_input(INPUT_POST, 'password');
-if (!empty($username)){
-if (!empty($password)){
 $host = "localhost";
 $dbusername = "root";
 $dbpassword = "";
@@ -26,15 +24,5 @@ echo "Error: ". $sql ."
 ". $conn->error;
 }
 $conn->close();
-}
-}
-else{
-echo "Password should not be empty";
-die();
-}
-}
-else{
-echo "Username should not be empty";
-die();
 }
 ?>
